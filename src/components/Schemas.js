da2c6418bcf7a2805f101bd7a348cf195ac1c927
Michael Ramos <mdramos8@gmail.com>
2016-11-10 14:51:31 -0500

    make field the drop target

--- src/components/Schemas.js
+++ src/components/Schemas.js
@@ -1,26 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import { DropTarget } from 'react-dnd';
-import { DndTypes } from './shared/Constants';
+
 import Accordion from './shared/Accordion';
-
-const schemaTarget = {
-  canDrop(props) {
-    // STUB (will rely on schema rules)
-    return true;
-  },
-
-  drop(props) {
-    console.log(props)
-  }
-};
-
-function collect(connect, monitor) {
-  return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver(),
-    canDrop: monitor.canDrop()
-  };
-}
+import SchemaField from './schemas/schemaField';
 
 import '../styles/Schemas.css';
 
@@ -29,15 +10,13 @@ class Schemas extends Component {
   
   
   baseViewRender(schema) {
-    return (
-      //column_name":"fid","nullable":"False","primary_key":true,"type":"INTEGER" 
+    return(
       <div>
         {schema.properties.map((field,i) =>
-          <ul>
-            <li><b>Column Name</b> {field.column_name}</li>
-            <li><b>Required</b> {field.nullable}</li>
-            <li><b>Type</b> {field.type}</li>
-          </ul>
+          <SchemaField
+            key={i}
+            field={field}
+          />
         )}
       </div>
     );
@@ -46,15 +25,10 @@ class Schemas extends Component {
   render() {
     const {
       availableSchemas,
-      activeSchemaId,
-
-      connectDropTarget,
-      isOver
+      activeSchemaId
     } = this.props;
 
-    
-
-    return connectDropTarget(
+    return (
       <div className="Schemas">
         <div className="Schemas-view">
 
@@ -76,10 +50,7 @@ Schemas.propTypes = {
   activeSchemaId: React.PropTypes.oneOfType([
     React.PropTypes.string,
     React.PropTypes.number
-  ]),
-
-  // Injected by React DnD:
-  connectDropTarget: PropTypes.func.isRequired
+  ])
 };
 
-export default DropTarget(DndTypes.HEADER, schemaTarget, collect)(Schemas);
+export default Schemas;
---
+++ src/components/schemas/schemaField.js
@@ -0,0 +1,46 @@
+import React, {Component, PropTypes} from 'react';
+
+import { DropTarget } from 'react-dnd';
+import { DndTypes } from '../shared/Constants';
+
+const schemaTarget = {
+  canDrop(props) {
+    // STUB (will rely on schema rules)
+    return true;
+  },
+
+  drop(props) {
+    console.log(props)
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  };
+}
+
+class SchemaField extends Component {
+  render() {
+    const {
+      field,
+      
+      connectDropTarget,
+      isOver
+    } = this.props;
+
+    return connectDropTarget(
+      <span><b>Column Name</b> {field.column_name}<b>Type</b> {field.type}<b>Required</b> {field.nullable}<br/></span>
+    );
+  }
+}
+
+SchemaField.propTypes = {
+
+  // Injected by React DnD:
+  connectDropTarget: PropTypes.func.isRequired
+};
+
+export default DropTarget(DndTypes.HEADER, schemaTarget, collect)(SchemaField);
